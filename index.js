96b8ae3d057441dc077e355febbc6cacd1dec10c
skylucacius <skylucacius@hotmail.com>
2020-05-09 19:10:46 -0300

    node instalado com Hello World

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Node rodando com sucesso!')
+const express = require('express')
+const app = express()
+const port = 3000
+app.get
+app.listen(port, () => console.log('App rodando na porta 3000!'))
