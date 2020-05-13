6c5601b72c40cf35dca15d925a668a1d0ea5d798
skylucacius <skylucacius@hotmail.com>
2020-05-13 15:50:29 -0300

    API funcional - tutorial backend em node

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.get
+const userRoute = require('./routes/userRoute')
+app.get('/', (req,res) => {
+    res.send('Olá mundo pelo Express!')
+})
 app.listen(port, () => console.log('App rodando na porta 3000!'))
